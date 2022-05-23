aeb7d399964c3a0c9695b6c13daaf628852b4264
roshansw <roshanwalzade2@gmail.com>
2022-05-23 22:25:47 +0530

    IS_PART_TIME = 4

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -3,16 +3,19 @@ public class EmpWage {
 
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOURS = 20;
 
         int empHrs = 0;
         int empWage = 0;
 
 
-        int empCheck = (int) (Math.random()*10)%2;
+        int empCheck = (int) (Math.random()*10)%3;
 
         if(empCheck == IS_FULL_TIME)
             empHrs = 8;
+        else if (empCheck == IS_PART_TIME)
+            empHrs = 4;
         else
             empHrs = 0;
 
